fec5f0640225e3b44a4cf54af163d112a7c02e8d
charleslukes <charleslukes28@gmail.com>
2022-02-06 08:47:37 +0100

    add generic meta data

---
+++ utils/nftport/generic_metas.js
@@ -0,0 +1,32 @@
+import path from "path";
+import fs from "fs";
+
+const basePath = process.cwd();
+const buildDir = path.join(basePath, "/build");
+
+const GENERIC_TITLE = "Ethbuild Endgame" 
+const GENERIC_DESCRIPTION = "This the nft for the ethbuild endgame team" 
+
+if (!fs.existsSync(path.join(buildDir, "/genericJson"))) {
+  fs.mkdirSync(path.join(buildDir, "/genericJson"));
+}
+
+fs.readdirSync(`${buildDir}/json`).forEach((file) => {
+  if (file === "_metadata.json" || file === "_ipfsMetas.json") return;
+
+  const jsonFile = JSON.parse(fs.readFileSync(`${buildDir}/json/${file}`));
+
+  jsonFile.name = `${GENERIC_TITLE} #${jsonFile.custom_fields.edition}`;
+  jsonFile.description = GENERIC_DESCRIPTION;
+  jsonFile.file_url =
+    "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh";
+  delete jsonFile.attributes;
+  delete jsonFile.custom_fields.dna;
+
+  fs.writeFileSync(
+    `${buildDir}/genericJson/${file}`,
+    JSON.stringify(jsonFile, null, 2)
+  );
+
+  console.log(`${file} copied and updated!`);
+});
